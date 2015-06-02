8165fef123abf2d0df12134212b657d7244772e6
Ben Morrow <childoftv@gmail.com>
2015-06-01 19:12:24 -0700

    master test harness

---
+++ engine/tests/build.sh
@@ -0,0 +1,5 @@
+# NOTE:  to run app on OSX you will need:
+# export DYLD_FALLBACK_LIBRARY_PATH=/Users/you/code/electrifying-stunt/engine/lib
+# or you can:
+# cp ../engine/lib/libengine.so .
+g++ -L../lib -I../include -Wall -o main main.cpp -lengine --std=c++11
---
+++ engine/tests/main.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch/catch.hpp"
+#include "../include/component.h"
+
+TEST_CASE( "Can create a component", "[Component]" ) {
+
+   Component *c= new Component();
+}
