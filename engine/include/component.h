85d3c6f60fcb7187e9d963329ab815d745d8c17b
Ben Morrow <childoftv@gmail.com>
2015-06-01 18:30:19 -0700

    master improve comments and interface, add TODOs

--- engine/include/component.h
+++ engine/include/component.h
@@ -7,17 +7,27 @@
 #include "color.h"
 
 /**
- * Components are the building blocks of the Symmetry Engine. They receive inputs, and provide outputs.
+ * Components are the building blocks of the Symmetry Engine. They receive inputs, and provide 
+ * outputs.
  * A component can contain other components.
  */
 class Component {
 public:
+	// TODO need a list of inputs
+    // TODO need a list of outputs
+    // TODO need methods for adding and retrieving inputs and outputs
+
 	Component();
 	~Component();
 	
-	Color* calculate(Pixel *pixel, Group *topLevel /*, frameContext */);
+	/**
+     * Tells the component to update its model according to the current time
+    */
 	void update(double time);
-
+	/**
+     * Called by another component to finally render a color based on component state
+    */
+	Color* calculate(Pixel *pixel, Group *topLevel /*, frameContext */);
 };
 
 
--- testapp/build.sh
+++ testapp/build.sh
@@ -1,4 +1,5 @@
-# NOTE:  to run app you will need
-# export DYLD_FALLBACK_LIBRARY_PATH=/Users/ben/code/electrifying-stunt/engine/lib
-# or you can copy the engine/lib/libengine.so to .
+# NOTE:  to run app on OSX you will need:
+# export DYLD_FALLBACK_LIBRARY_PATH=/Users/you/code/electrifying-stunt/engine/lib
+# or you can:
+# cp ../engine/lib/libengine.so .
 g++ -L../engine/lib -I../engine/include -Wall -o app app.cpp -lengine --std=c++11
