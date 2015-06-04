ee948947930bdef517a6bc8037eca1e48f85bd4e
Ben Morrow <childoftv@gmail.com>
2015-06-03 19:08:43 -0700

    <master> move shit around, improve makefile

--- engine/tests/main.cpp
+++ engine/tests/main.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch/catch.hpp"
-#include "../include/component.h"
+#include "component.h"
 
 TEST_CASE( "Can create a component", "[Component]" ) {
 
