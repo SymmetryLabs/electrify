19d47a3c29b8be63e1e19243e0d7a3070769db67
Ben Morrow <childoftv@gmail.com>
2015-06-03 17:38:28 -0700

    <master> rename repo stuff

--- testapp/build.sh
+++ testapp/build.sh
@@ -1,5 +1,5 @@
 # NOTE:  to run app on OSX you will need:
-# export DYLD_FALLBACK_LIBRARY_PATH=<absolute path to...>/electrifying-stunt/engine/lib
+# export DYLD_FALLBACK_LIBRARY_PATH=<absolute path to...>/electrify/engine/lib
 # or you can:
 # cp ../engine/lib/libengine.so .
 g++ -L../engine/lib -I../engine/include -Wall -o app app.cpp -lengine --std=c++11
