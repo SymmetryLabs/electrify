76d531ab74a91584247d4eb2b554f5839dbaca67
Ben Morrow <childoftv@gmail.com>
2015-06-04 22:20:16 -0700

    master generic signals (using lambdas to wrap crazy member functions and thus be able to store them... :/ )

--- testapp/app.cpp
+++ testapp/app.cpp
@@ -1,8 +1,16 @@
+#include "app.h"
 #include <iostream>
-#include "component.h"
+#include <string>
+#include "constant_color_component.h"
+#include "signals.h"
+#include "pixel.h"
+#include "group.h"
+
+
  int main()
  {
- 	Component *c= new Component();
- 	std::cout << "Hello Testapp\n";
+ 	ConstantColorComponent *c= new ConstantColorComponent();
+ 	std::string color("color");
+ 	std::cout << ((ColorSignal*) c->outputs[color])->calculate_function(new Pixel(),new Group())->test;
  	return 0;
  }
