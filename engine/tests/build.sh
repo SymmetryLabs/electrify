1703c28475c59c9feea62f8a908fd57b2ebe1f94
Kyle Fleming <garthex@gmail.com>
2015-06-08 20:51:54 -0700

    make it so build.sh can be called from any folder

--- engine/tests/build.sh
+++ engine/tests/build.sh
@@ -2,4 +2,5 @@
 # export DYLD_FALLBACK_LIBRARY_PATH=<absolute path to...>/electrify/engine/lib
 # or you can:
 # cp ../lib/libengine.so .
+cd "$( dirname "$0" )"
 g++ -L../lib -I../src/ -Wall -o main main.cpp -lengine --std=c++11
--- testapp/build.sh
+++ testapp/build.sh
@@ -2,4 +2,5 @@
 # export DYLD_FALLBACK_LIBRARY_PATH=<absolute path to...>/electrify/engine/lib
 # or you can:
 # cp ../engine/lib/libengine.so .
+cd "$( dirname "$0" )"
 g++ -L../engine/lib -I../engine/src/ -Wall -o app app.cpp -lengine --std=c++11
