7ba3157c5adad8b1e57907b9a5c6922ded01446f
Ben Morrow <childoftv@address.com>
2015-07-15 19:45:40 -0700

    update test code

--- engine/tests/build.sh
+++ engine/tests/build.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 # NOTE:  to run app on OSX you will need:
 # export DYLD_FALLBACK_LIBRARY_PATH=<absolute path to...>/electrify/engine/lib
 # or you can:
 # cp ../lib/libengine.so .
 cd "$( dirname "$0" )"
-g++ -L../lib -I../src/ -Wall -o main main.cpp -lengine --std=c++11
+g++ -L../build -I../build/include -o main main.cpp -lSymmetryEngine --std=c++11
