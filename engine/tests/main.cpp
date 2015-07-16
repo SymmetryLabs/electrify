4dd737bb6e73345df312d8a88996022012535bd2
Ben Morrow <childoftv@gmail.com>
2015-07-16 14:37:23 -0700

    <master> tests to refer to main build folder

--- engine/tests/main.cpp
+++ engine/tests/main.cpp
@@ -1,8 +1,11 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch/catch.hpp"
-#include "basic_component.h"
+#include "component.h"
 
-TEST_CASE( "Can create a component", "[Component]" ) {
+SCENARIO( "Create and Wire a component", "[component]" ) {
 
-   Component *c= new Component();
+  GIVEN( "A Component" ) {
+    Component *c= new Component();
+    REQUIRE(c)
+  }
 }
