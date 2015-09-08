7717affd01a2c0870a533bac85daf0f134507796
Kyle Fleming <garthex@gmail.com>
2015-09-07 23:20:58 -0700

    delete electrifyapp and designerqt

--- designerqt/component_list_qt.cpp
+++
@@ -1,9 +0,0 @@
-#include "component_list_qt.h"
-
-ComponentListQt::ComponentListQt(QWidget *parent) : QListWidget(parent)
-{
-  for (auto componentName : componentRegistrar.getAvailableComponents()) {
-    addItem(QString::fromStdString(componentName));
-  }
-}
-
